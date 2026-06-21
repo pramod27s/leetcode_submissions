a79c30c6dde64d8263d73ba157978498057c9a28
Pramod M B <mbpramod3@gmail.com>
2026-06-21 12:49:47 +0530

    Add: perfect-squares - submission-2

---
+++ Data Structures & Algorithms/perfect-squares/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    int dp[];
+
+    public int numSquares(int n) {
+        dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - j * j]);
+            }
+        }
+        
+        return dp[n];
+    }
+}
